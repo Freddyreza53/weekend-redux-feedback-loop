a8d3c1ed26e2ae2f0bda2416a40e50b259e51b99
Freddy Reza <rezafreddy@gmail.com>
2022-04-02 15:38:38 -0500

    add edit capabilities, need to be fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,42 +10,58 @@ import CommentsForm from '../CommentsForm/CommentsForm';
 import ReviewForm from '../ReviewForm/ReviewForm';
 import SubmittedPage from '../SubmittedPage/SubmittedPage';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createTheme({
+    palette: {
+        type: 'light',
+        primary: {
+        main: '#34cebf',
+        },
+        secondary: {
+        main: '#f90707',
+        },
+    },
+});
 
 function App() {
 
   return (
-    <Router>
-      <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Feedback!</h1>
-          <h4>Don't forget it!</h4>
-        </header>
-        <Route path="/" exact>
-          <FeelingForm />
-        </Route>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div className='App'>
+          <header className='App-header'>
+            <h1 className='App-title'>Feedback!</h1>
+            <h4>Don't forget it!</h4>
+          </header>
+          <Route path="/" exact>
+            <FeelingForm />
+          </Route>
 
-        <Route path="/understandingForm">
-          <UnderstandingForm />
-        </Route>
+          <Route path="/understandingForm">
+            <UnderstandingForm />
+          </Route>
 
-        <Route path="/supportForm">
-          <SupportForm />
-        </Route>
+          <Route path="/supportForm">
+            <SupportForm />
+          </Route>
 
-        <Route path="/commentsForm">
-          <CommentsForm />
-        </Route>
+          <Route path="/commentsForm">
+            <CommentsForm />
+          </Route>
 
-        <Route path="/reviewForm">
-          <ReviewForm />
-        </Route>
+          <Route path="/reviewForm">
+            <ReviewForm />
+          </Route>
 
-        <Route path="/submittedPage">
-          <SubmittedPage />
-        </Route>
+          <Route path="/submittedPage">
+            <SubmittedPage />
+          </Route>
 
-      </div>
-    </Router>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -3,6 +3,13 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
 
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
+import React, { useState } from 'react';
+import Box from '@material-ui/core/Box';
+
+
+
 function ReviewForm() {
 
     const history = useHistory();
@@ -38,13 +45,73 @@ function ReviewForm() {
         history.push('/commentsForm');
     }
 
+    const [editFeeling, setEditFeeling] = useState(false)
+
+
+    const editClick = () => {
+        setEditFeeling(!editFeeling);
+    }
+
+    const saveFeeling = () => {
+        console.log('yes');
+    }
+
     return (
         <div>
             <h2>Review Your Feedback</h2>
-            <h3>Feelings: {feelingInput}</h3>
-            <h3>Understanding: {understandingInput}</h3>
-            <h3>Support: {supportInput}</h3>
-            <h3>Comments: {commentInput}</h3>
+
+            <Box style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                {editFeeling === true ? 
+                    <div className="editDiv">
+                        <h3>Feelings: </h3>
+                        <input className="editInput" type="number" onChange={saveFeeling} value=''/> 
+                        <button onClick={saveFeeling}>save</button>
+                    </div>
+                    : 
+                    <h3>Feelings: {feelingInput} </h3>}
+                
+
+                {/* {editFeeling && <button>save</button> } */}
+                
+                
+                <IconButton color="secondary" onClick={editClick}>
+                    <EditIcon />
+                </IconButton>
+            </Box>
+
+            <Box style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                <h3>Understanding: {understandingInput}</h3>
+                <IconButton color="secondary" >
+                    <EditIcon />
+                </IconButton>
+            </Box>
+
+            <Box style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                <h3>Support: {supportInput}</h3>
+                <IconButton color="secondary" >
+                    <EditIcon />
+                </IconButton>
+            </Box>
+
+            <Box style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                <h3>Comments: {commentInput}</h3>
+                <IconButton color="secondary">
+                    <EditIcon />
+                </IconButton>
+            </Box>
+            
             <div className="buttonDiv">
                 <button onClick={handleBack}>BACK</button>
                 <button onClick={handleClick}>NEXT</button>

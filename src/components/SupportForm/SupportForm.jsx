947aa65b89d0e901bbcbc7e08b2cb97dd02070c7
Freddy Reza <rezafreddy@gmail.com>
2022-04-02 14:13:21 -0500

    add stars rating to support page.

--- src/components/SupportForm/SupportForm.jsx
+++ src/components/SupportForm/SupportForm.jsx
@@ -1,20 +1,43 @@
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
+
+const labels = {
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+};
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+});
 
 function SupportForm() {
+    const [value, setValue] = React.useState(0);
+    const [hover, setHover] = React.useState(-1);
+    const classes = useStyles();
 
     const history = useHistory();
     const dispatch = useDispatch();
     let [supportInput, setSupportInput] = useState('');
 
     const handleClick = () => {
-        if (supportInput === '') {
+        if (value === 0) {
             alert('Please add a number between 1-5.')
         } else {
             dispatch({
                 type: 'ADD_SUPPORT',
-                payload: supportInput
+                payload: value
             })
             history.push('/commentsForm');
         }
@@ -24,20 +47,33 @@ function SupportForm() {
         history.push('/understandingForm');
     }
 
-
-    const addFeedback = (event) => {
-        setSupportInput(event.target.value);
-    };
+    // const addFeedback = (event) => {
+    //     setSupportInput(event.target.value);
+    // };
 
     return (
-        <div>
+        <div className={classes.root}>
             <h1>How well are you being supported?</h1>
             <label>Support scale 1-5: </label>
-            <input 
+            {/* <input 
                 type="number" 
                 onChange={addFeedback}
                 value={supportInput}
-            />
+            /> */}
+            
+                <Rating
+                    name="hover-feedback"
+                    value={value}
+                    precision={1}
+                    onChange={(event, newValue) => {
+                    setValue(newValue);
+                    }}
+                    onChangeActive={(event, newHover) => {
+                    setHover(newHover);
+                    }}
+                />
+                {value !== null && <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>}
+            
             <div>
                 <button onClick={handleBack}>BACK</button>
                 <button onClick={handleClick}>NEXT</button>
